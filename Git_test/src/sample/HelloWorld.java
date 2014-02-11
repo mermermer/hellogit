35a211bffce8da9031bfb4bb41e1dbd11ec4fc92
Motoyuki Sakai_ <Motoyuki Sakai_@maya.mshome.net>
2014-02-12 00:25:43 +0900

    a

--- Git_test/src/sample/HelloWorld.java
+++ Git_test/src/sample/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("b_HelloWorld_a");
+		System.out.println("b_HelloWorld_a");
+		System.out.println("b_HelloWorld_a");
+		System.out.println("b_HelloWorld_a");
 	}
 
 }
