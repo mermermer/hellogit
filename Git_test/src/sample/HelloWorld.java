db2ecfa50579dbe9388099ca6501b6f5c5b3968c
Motoyuki Sakai_ <Motoyuki Sakai_@maya.mshome.net>
2014-02-12 00:18:44 +0900

    a

--- Git_test/src/sample/HelloWorld.java
+++ Git_test/src/sample/HelloWorld.java
@@ -3,7 +3,8 @@ package sample;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("b_HelloWorld");
+		System.out.println("b_HelloWorld_a");
+		System.out.println("b_HelloWorld_a");
 	}
 
 }
